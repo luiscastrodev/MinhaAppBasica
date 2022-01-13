4c0665dec9ad0efb3214175d2da49efaa42aa483
Luis Ricardo <luisricardodecastro@gmail.com>
2022-01-13 10:08:46 -0300

    Initial commit

---
+++ MinhaAppBasica/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MinhaAppBasica.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();    
+        }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ MinhaAppBasica/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace MinhaAppBasica.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
